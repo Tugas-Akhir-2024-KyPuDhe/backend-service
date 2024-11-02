022c2d540cae076bba3f4b43fbd4ec6b7338f5ac
MhmmdSyahputra <putra.cokers@gmail.com>
2024-11-02 15:48:00 +0700

    add dotenv in index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const ekstrakurikulerRoutes = require("./routes/ekstrakurikulerRoutes");
 const cors = require("cors");
 
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.use(cors());
 app.use(bodyParser.json());
 
