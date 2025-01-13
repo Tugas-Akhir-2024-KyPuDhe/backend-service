70bcc3c37c9cd07dd25fab31415578e4979c00c3
MhmmdSyahputra <putra.cokers@gmail.com>
2025-01-13 14:32:36 +0700

    add access position

--- src/routes/studentPositionInClassRoutes.js
+++ src/routes/studentPositionInClassRoutes.js
@@ -4,7 +4,7 @@ const { authMiddleware, authorizeRoles } = require("../middlewares/authMiddlewar
 const router = express.Router();
 
 router.post("/store", authMiddleware, authorizeRoles(['STAFF', 'TEACHER']), studentPositionInClassController.addStudentPosition);
-router.delete("/get-byClassId/:id", authMiddleware, authorizeRoles(['STAFF', 'TEACHER']), studentPositionInClassController.getPosisitionByClassId);
+router.get("/get-byClassId/:id", authMiddleware, authorizeRoles(['STAFF', 'TEACHER', 'STUDENT']), studentPositionInClassController.getPosisitionByClassId);
 router.delete("/delete/:id", authMiddleware, authorizeRoles(['STAFF', 'TEACHER']), studentPositionInClassController.deleteStudentPosition);
 
 module.exports = router;
