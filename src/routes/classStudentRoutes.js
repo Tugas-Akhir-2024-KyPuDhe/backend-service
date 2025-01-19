950c153e3a2f876a8c9901bf8b021520333c38f3
MhmmdSyahputra <putra.cokers@gmail.com>
2025-01-19 14:12:14 +0700

    add autzho for student in class

--- src/routes/classStudentRoutes.js
+++ src/routes/classStudentRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // router.post("/createClassInvStudent", authMiddleware, authorizeRoles(['STAFF']), classStudentController.createClassInvStudent);
 router.put("/insert-stundent-inclass", authMiddleware, authorizeRoles(['STAFF']), classStudentController.insertStudentInClass);
-router.get("/get", authMiddleware, authorizeRoles(['STAFF', 'TEACHER']), classStudentController.getAllClass);
+router.get("/get", authMiddleware, authorizeRoles(['STAFF', 'TEACHER', 'STUDENT']), classStudentController.getAllClass);
 router.get("/get/:id", authMiddleware, authorizeRoles(['STAFF', 'TEACHER']), classStudentController.getClassById);
 router.put("/update/:id", authMiddleware, authorizeRoles(['STAFF']), classStudentController.updateClass);
 router.post("/store", authMiddleware, authorizeRoles(['STAFF']), classStudentController.createClass);
