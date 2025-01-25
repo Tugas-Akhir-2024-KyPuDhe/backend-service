f3841af3e1c0740f87ce034d81db9bf1a5218baf
MhmmdSyahputra <putra.cokers@gmail.com>
2025-01-26 01:36:40 +0700

    change controller

--- src/routes/studentAttendanceRoutes.js
+++ src/routes/studentAttendanceRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get("/get/", authMiddleware, authorizeRoles(['STUDENT', 'TEACHER']), studentAttendanceController.getAttendance);
 router.post("/store/", authMiddleware, authorizeRoles(['STUDENT', 'TEACHER']), studentAttendanceController.createAttendance);
 router.put("/update/:attendanceId", authMiddleware, authorizeRoles(['STUDENT', 'TEACHER']), studentAttendanceController.updateAttendance);
-router.put("/update/final/:attendanceId", authMiddleware, authorizeRoles(['STUDENT', 'TEACHER']), studentAttendanceController.updateAttendance);
+router.put("/update/final/:attendanceId", authMiddleware, authorizeRoles(['STUDENT', 'TEACHER']), studentAttendanceController.updateStatusAttendance);
 
 module.exports = router;
