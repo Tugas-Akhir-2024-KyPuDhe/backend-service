6de208ce1aca899c5667d1221b3fce6b65f1d5ff
MhmmdSyahputra <putramhmmd22@gmail.com>
2025-03-27 21:56:33 +0700

    add listen ip

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 6000;
-app.listen(PORT, '0.0.0.0' () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
